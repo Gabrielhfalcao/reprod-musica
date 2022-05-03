5596c345206f74f14de8f644ace4fb0cc105ea09
Gabrielhfalcao <99846326+Gabrielhfalcao@users.noreply.github.com>
2022-05-03 10:40:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ console.log(divParaExpandir.offsetHeight);
 
 botao.addEventListener("click", (e) => {
   if (divParaExpandir.offsetHeight == 620) {
-    divParaExpandir.style.height = `${expandir + 420}px`;
+    divParaExpandir.style.height = `${expandir + 400}px`;
     console.log(divParaExpandir.offsetHeight);
   } else {
-    divParaExpandir.style.height = `${divParaExpandir.offsetHeight - 420}px`;
+    divParaExpandir.style.height = `${divParaExpandir.offsetHeight - 400}px`;
     console.log(expandir);
   }
 });
