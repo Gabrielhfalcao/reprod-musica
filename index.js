4c52c7e53016c1471e656b774e0ca23a127474fe
Gabrielhfalcao <99846326+Gabrielhfalcao@users.noreply.github.com>
2022-05-07 11:35:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,39 @@ botao.addEventListener("click", (e) => {
     console.log(expandir);
   }
 });
+
+var button1 = document.getElementById('button-ver-mais1');
+
+function entrar1(){
+  button1.style.backgroundColor = 'gray'
+}
+function mudarcor1(){
+  button1.style.backgroundColor = 'darkgray';
+}
+function voltacor1(){
+  button1.style.backgroundColor = 'black';
+}
+
+var button2 = document.getElementById('button-ver-mais2');
+
+function entrar2(){
+  button2.style.backgroundColor = 'gray'
+}
+function mudarcor(){
+  button2.style.backgroundColor = 'darkgray';
+}
+function voltacor(){
+  button2.style.backgroundColor = 'black';
+}
+
+var button3 = document.getElementById('button-ver-mais3');
+
+function entrar3(){
+  button3.style.backgroundColor = 'gray'
+}
+function mudarcor3(){
+  button3.style.backgroundColor = 'darkgray';
+}
+function voltacor3(){
+  button3.style.backgroundColor = 'black';
+}
